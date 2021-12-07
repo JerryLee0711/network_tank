ad2064436e2cf7c81cc66a192b3774e8b83a6484
JerryLee <mouse850711@gmail.com>
2021-12-07 20:54:36 +0800

    modify join room function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,7 +58,15 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("Joined room!!");
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Created room!!");
+            PhotonNetwork.LoadLevel("GameScene");
+        }
+        else
+        {
+            Debug.Log("Joined room!!");
+        }
     }
 
     public override void OnJoinRoomFailed(short returnCode,string message)
