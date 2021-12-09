7c3d178d3cab425271a61a668d43976134656bec
JerryLee <mouse850711@gmail.com>
2021-12-09 20:06:57 +0800

    modify mainmenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,10 +2,14 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviourPunCallbacks
 {
     static MainMenu instance;
+    private GameObject m_ui;
+    private Button m_joinGameButton;
+
     void Awake()
     {
         if (instance != null)
@@ -15,5 +19,12 @@ public class MainMenu : MonoBehaviourPunCallbacks
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
+    
+
+        m_ui = transform.FindAnyChild<Transform>("UI").gameObject;
+        m_joinGameButton = transform.FindAnyChild<Button>("JoinGameButton");
+        m_ui.SetActive(true);
+        m_joinGameButton.interactable = false;
     }
+
 }
