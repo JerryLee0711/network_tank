96dadf622476edf79c00a443541b4e8313c7ab16
JerryLee <mouse850711@gmail.com>
2021-12-09 21:19:57 +0800

    disable button when change scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviourPunCallbacks
@@ -27,4 +28,25 @@ public class MainMenu : MonoBehaviourPunCallbacks
         m_joinGameButton.interactable = false;
     }
 
+    public override void OnConnectedToMaster()
+    {
+        m_joinGameButton.interactable = true;
+    }
+
+    public override void OnEnable()
+    {
+        // Always call the base to add callbacks
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    public override void OnDisable()
+    {
+        // Always call the base to remove callbacks
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        m_ui.SetActive(!PhotonNetwork.InRoom);
+    }
 }
